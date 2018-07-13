2566c3f4462cc1793f5455de3a00092ba326915c
bchiu42 <bchiu42@gmail.com>
2018-07-13 12:16:01 -0400

    Add files via upload

---
+++ Snek/src/Board.java
@@ -0,0 +1,10 @@
+
+public class Board {
+  private Node[][] boards;
+  private Snake Snek;
+  private final static int height = 10;   
+  private final static int width = 10; 
+  public board(){
+	  
+  }
+}
